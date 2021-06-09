fa17b03f02c8aabd31426c6cb478a48464e2b766
Oracle Public Cloud User <opc@jenkins.publicsubnet1.ractest.oraclevcn.com>
2021-06-09 02:02:10 +0000

    add function

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ const app = express()
 var util = require('./calc.js')
 
 app.get('/', (req, res) => res.send(util.message()))
+app.get('add', (req, res) => res.send("sum: "+util.add(req.query.x, req.query.y)))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
