eaa6933a70238be633f87ccb5512170e417a5ec2
Oracle Public Cloud User <opc@jenkins.publicsubnet1.ractest.oraclevcn.com>
2021-06-09 04:23:50 +0000

    bug fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express()
 var util = require('./calc.js')
 
 app.get('/', (req, res) => res.send(util.message()))
-app.get('/add', (req, res) => res.send("sum: "+util.add(req.query.x, req.query.y)))
+app.get('/add', (req, res) => res.send("sum: "+util.add(Number(req.query.x), Number(req.query.y))))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
